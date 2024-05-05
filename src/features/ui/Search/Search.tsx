3b7c75a7fedfeb32653e362340785dc6f2fcb3da
Lisin Andrey <lisin.av@glonasssoft.ru>
2024-05-05 14:14:06 +0300

    Search component

---
+++ src/features/ui/Search/Search.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { Input } from 'shared/ui/Input'
+
+export const Search = () => {
+    const [searchValue, setSearchValue] = useState<string>('')
+  return (
+    <div>
+        <Input value={searchValue} onChange={setSearchValue}/>
+    </div>
+  )
+}
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import cls from "./SideBar.module.scss";
+import cls from "./NavBar.module.scss";
 import { Link } from "react-router-dom";
+import { Search } from "features/ui";
 
-export const SideBar = () => {
+export const NavBar = () => {
   return (
-    <div className={cls.sideBar}>
+    <div className={cls.navBar}>
       <Link to={"/"}>Home</Link>
       <Link to={"/cast"}>Cast</Link>
       <Link to={"/characters"}>Characters</Link>
       <Link to={"/info"}>Info</Link>
       <Link to={"/episodes"}>Episodes</Link>
       <Link to={"/inventory"}>Inventory</Link>
+      <Search />
     </div>
   );
 };
